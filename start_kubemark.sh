531aa4aec9ae2c302fa673ae5feb84741683151f
zhouwei9408 <zhouwei9408@gmail.com>
2024-09-26 09:35:33 +0800

    init

---
+++ start_kubemark.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+kubectl create configmap "node-configmap" --namespace="kubemark" --from-file=kernel.monitor="kernel-monitor.json"
+
+kubectl create secret generic "kubeconfig" --type=Opaque --namespace="kubemark" \
+--from-file=kubelet.kubeconfig=/root/.kube/config  \
+--from-file=kubeproxy.kubeconfig=/root/.kube/config  \
+--from-file=npd.kubeconfig=/root/.kube/config  \
+--from-file=heapster.kubeconfig=/root/.kube/config  \
+--from-file=cluster_autoscaler.kubeconfig=/root/.kube/config  \
+--from-file=dns.kubeconfig=/root/.kube/config
+
